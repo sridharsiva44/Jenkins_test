bc2778161f392bd5075ec0c5f0181501dcbe7083
sridharsiva44 <r.s.sridharvenkat@gmail.com>
2022-09-15 12:03:18 +0530

    Create simple maven project in eclipse and push to Git-hub repo.

---
+++ Git/src/main/java/Git_Hub/Git/App.java
@@ -0,0 +1,13 @@
+package Git_Hub.Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
